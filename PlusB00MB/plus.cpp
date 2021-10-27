fd3866d5108caa588d55a0b13d529d4b25f7a687
DeVeL_AnOnIk <27363518+anonik9900@users.noreply.github.com>
2021-10-27 11:58:19 +0200

    improved source

--- PlusB00MB/plus.cpp
+++ PlusB00MB/plus.cpp
@@ -37,8 +37,7 @@ float risultato1;
 float risultato2;
 float risultato3;
 
-int mnp = 20;
-float mnp_val = 7.99;
+
 
 //Dichiariamo il main
 
@@ -55,21 +54,21 @@ int main ()
 
     cout<<"1) What is your name? "<<endl;
     cin>>risposta1;
-    risultato1 = 0210240204; //primo valore per overflow
+
 
     cout<<"2) Who was the 1st  President of America?"<<endl;
     cin>>risposta2;
-    risultato2 = 9354935935; //secondo valore per overflow
+
 
     cout<<"3) What are you doing now ?"<<endl;
     cin>>risposta3;
-    risultato3 = 4953095359; //terzo valore per overflow
+
 
     cout<<"\n \n"<<endl;
     cout<<"Calculating the result, please Wait! .... \n"<<endl;
 
     float pter = (risultato1*risultato2*risultato3);
-    float tot_mnp = (mnp_val*mnp);
+
     //Adesso inizio il loop
 
     ofstream fout("final.txt"); //creo fout e il file di testo
@@ -78,7 +77,7 @@ int main ()
     cin>>n;
 
 
-    if (n==0) //se il numero inserito è 0
+    if (n==0) //se il numero inserito Ã¨ 0
     {
         int n = 1; //aumento automaticamente il valore a 1, in modo da far funzionare il loop
         while(n!=0)
